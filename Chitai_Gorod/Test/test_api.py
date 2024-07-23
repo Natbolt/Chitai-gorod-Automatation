c2033b04088656a328dd589719a8901fb7c6f9dd
Natbolt <edusmart9696@yandex.ru>
2024-07-24 01:49:51 +0300

    final  check

--- Chitai_Gorod/Test/test_api.py
+++ Chitai_Gorod/Test/test_api.py
@@ -5,30 +5,30 @@ api = ShopAPI('https://web-gate.chitai-gorod.ru/api/')
 
 
 @allure.id("SKYPRO-1")
-@allure.epic("Корзина Читай-городю API")
+@allure.epic("Корзина Читай-город. API")
 @allure.feature("ADD")
 @allure.title("Добавление одного товара в корзину")
 @allure.severity("blocker")
 @allure.suite("API тесты на работу с корзиной")
 def test_1():
-    id = api.find_good('Удивительная девочка')["id"]
+    id = api.find_good('Идеальный Че, Интуиция и новые беспринцыпные истории')["id"]
     api.add_to_cart(id)
     id_added = api.get_short_cart()["items"][0]
     with allure.step("Очистка тестового пространства: Удаление товара из корзины"):
         delid = api.get_cart()["products"][0]["id"]
         api.delete_goods(delid)
-    with allure.step("Проверить, что id товара в корзине совпадает с id выбранного для добавления в коразину товара"):
+    with allure.step("Проверить, что id товара в корзине совпадает с id выбранного для добавления в корзину товара"):
         assert id == id_added
 
 @allure.id ("SKYPRO-2")
 @allure.epic("Корзина Читай-город. АРТ")
-@allure.story ("Измунение количества товаров в корзине")
+@allure.story ("Изменение количества товаров в корзине")
 @allure.feature("UPDATE")
 @allure.title("увеличение количества единиц одного товара в корзине")
 @allure.severity("critical")
 @allure. suite("API тесты на работу с корзиной")
 def test_2():
-    id = api.find_good("Удивительная девочка")["id"]
+    id = api.find_good('Идеальный Че, Интуиция и новые беспринцыпные истории')["id"]
     api.add_to_cart(id)
     cart_id = api.get_cart()["products"][0]["id"]
     new_q = 4
@@ -38,7 +38,7 @@ def test_2():
     with allure.step("Проверить, что число единиц товаров изменилось в соответствии в установленным значением"):
         assert new_q == q_in_cart
 
-@allure.id ("sKYPRO-3")
+@allure.id ("SKYPRO-3")
 @allure.epic("Корзина Читай-город. АРІ")
 @allure.story("Удаление товара из корзины")
 @allure.feature("DELETE")
@@ -46,7 +46,7 @@ def test_2():
 @allure.severity("blocker")
 @allure. suite("API тесты на работу с корзиной")
 def test_3():
-    id = api.find_good("Удивительная девочка")["id"]
+    id = api.find_good('Идеальный Че, Интуиция и новые беспринцыпные истории')["id"]
     api.add_to_cart(id)
     del_id = api.get_cart ()["products"][0]["id"]
     result = api.delete_goods(del_id)
@@ -67,7 +67,7 @@ def test_3():
 @allure.suite("API тесты на работу с корзиной")
 def test_4():
     with allure.step("Добавить в корзину все товары из списка"):
-        goods = ['Настольная игра Каркассон, Hobby World', 'Настольная игра GaGa Games "Большая Бродилка', 'Акриловая краска орхидея olki, 100 мл']
+        goods = ['Пазл Premium "Искусство орнамента", 4000 элементов', 'Мягкая игрушка "Корги", 60 см', 'Акриловая краска орхидея olki, 100 мл']
         total_cost = 0
         for i in goods:
             id = api.find_good(i)["id"]
@@ -84,3 +84,38 @@ def test_4():
         assert total_sum == total_cost
     with allure.step("Проверить, что количество товаров в корзине совпадает в количеством добавленных товаров"):
         assert count == len(goods)
+
+@allure.id("SKYPRO-5")
+@allure.epic("Корзина Читай-город. АРІ")
+@allure. story ("Добавление нескольких товаров в корзину с последующим удалением нескольких")
+@allure.feature("ADD")
+@allure.title("Добавление 3 товаров в корзину и Удаление двух из них")
+@allure.severity("critical")
+@allure.suite("API тесты на работу с корзиной")
+def test_5():
+    with allure.step("Добавить в корзину все товары из списка"):
+        goods = ['Пазл Premium "Искусство орнамента", 4000 элементов', 'Мягкая игрушка "Корги", 60 см', 'Акриловая краска орхидея olki, 100 мл']
+        total_cost = 0
+        for i in goods:
+            id = api.find_good(i)["id"]
+            price = api.find_good(i)["price"]
+            total_cost += api.find_good(i)["price"]
+            api.add_to_cart(id)
+    api.get_cart()
+    with allure.step("Посчитать количество товаров в корзине"):
+        count = len(api.get_short_cart()["items"])
+    with allure.step("Удалить первые два добавленных товара по id"):
+        del_goods_by_id = []
+        del_goods_by_id.append(api.get_cart()["products"][0]["id"])
+        del_goods_by_id.append(api.get_cart()["products"][1]["id"])
+        for id in del_goods_by_id:
+            result = api.delete_goods(id)
+        deleted_cart = api.get_short_cart()["quantity"]
+    with allure.step("Проверить, что список товаров в корзине после удаления равен 1"):
+        assert len(api.get_short_cart()["items"]) == 1
+    with allure.step("Проверить, что количество товаров равно 1"):
+        assert deleted_cart == 1
+    with allure.step("Проверить, статус-код ответа - 204"):
+        assert result == 204
+    with allure.step("Очистка тестового пространства: удаление товара из корзины"):
+        api.delete_goods(deleted_cart)
--- Chitai_Gorod/Test/test_ui.py
+++ Chitai_Gorod/Test/test_ui.py
@@ -6,66 +6,123 @@ import allure
 
 
 cookie = {
-    'name': 'accsess-token',
-    'value': 'Bearer%20eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIwODQzNTE4LCJpYXQiOjE3MjE0NTk2MDIsImV4cCI6MTcyMTQ2MzIwMiwidHlwZSI6MjB9.9FpaSv1T2HsROk9ZN5mUzv-vmyazpFmmIXt3xdVU7nc'
+    'name': 'access-token',
+    'value': 'Bearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjE4OTkyOTksImlhdCI6MTcyMTczMTI5OSwiaXNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6Ijk1MDYzYzNkN2ZlYWVmMzEzZTRjOTE1ZThjYzFmYjM4OWU1YTdlMjM0NTVmNmNiMzM5MzA5OGU4YjlkZDNhYmQiLCJ0eXBlIjoxMH0.Nc9WPfOvtJuUAog2NOpEvOsVyDPSOdnpXBipdPFucog'
 }
 
+@allure.id("SKYPRO-6")
+@allure.epic("Поиск Читай-город. UI")
+@allure.story("Поиск товара")
+@allure.feature("Find")
+@allure.title("Поиск товара через поисковую стороку")
+@allure.description("Поиск товара по артикулу через строку поиска")
+@allure.severity("critical")
+@allure.suite("UI тесты на работу с поиском")
 def test_find_book():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     main_page = MainPage(driver)
-    assert main_page.enter_values("2947069") == "Удивительная девочка"
+    book = main_page.enter_values("2824689")
+    assert book == 'Идеальный Че, Интуиция и новые беспринцыпные истории'
 
-@allure.id("SKYPRO-5")
+
+@allure.id("SKYPRO-7")
 @allure.epic("Корзина Читай-город. UI")
-@allure.story ("Добавление одного товара в корзину")
-@allure.feature ("ADD")
-@allure.title("Добавление одной книги в корзину")
-@allure. description("Добавление 1 книги в корзину с последующим изменением количества ее экземпляров и удалением из корзины")
+@allure.story("Добавление в корзину")
+@allure.feature("ADD")
+@allure.title("Добавление 1 книги в корзину")
+@allure.description("Добавление 1 книги в корзину")
+@allure.severity("blocker")
+@allure.suite("UI тесты на работу с корзиной")
+def test_add_book_to_cart():
+    with allure.step("Создать экземпляр класса и открыть браузер"):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        main_page = MainPage(driver)
+        # main_page.cookies(cookie) кукисы не пробрасывались, добавлялся почему-то второй ключ в Application, по факту авторизации нет
+    main_page.enter_values("Идеальный Че, Интуиция и новые беспринцыпные истории")
+    with allure.step("Добавить первую книгу из поисковой выдачи в корзину"):
+        dct = main_page.add_to_cart()
+    main_page.go_cart()
+    book = main_page.check_book()
+    with allure.step("Проверить, что в корзину добавилась нужная книга"):
+        assert book == dct["title"]
+
+
+@allure.id("SKYPRO-8")
+@allure.epic("Корзина Читай-город. UI")
+@allure.story("Удаление товара из корзины")
+@allure.feature("DEL")
+@allure.title("Удаление товара из корзины")
+@allure.description("Добавление в корзину 1 книги и последующее ее удаление из корзины")
+@allure.severity("critical")
+@allure.suite("UI тесты на работу с корзиной")
+def test_del_book():
+    with allure.step("Создать экземпляр класса и открыть браузер"):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        main_page = MainPage(driver)
+    main_page.enter_values("Идеальный Че, Интуиция и новые беспринцыпные истории")
+    with allure.step("Добавить первую книгу из поисковой выдачи в корзину"):
+        main_page.add_to_cart()
+    main_page.go_cart()
+    main_page.del_book()
+    clear_cart = main_page.check_quantity('0 товаров')
+    with allure.step("Проверить, книга удалилась из корзины"):
+        assert clear_cart == '0'
+
+
+@allure.id("SKYPRO-9")
+@allure.epic("Корзина Читай-город. UI")
+@allure.story("Добавление одного товара в корзину")
+@allure.feature("ADD")
+@allure.title("Добавление одной книги в корзину и изменение количества экземляров")
+@allure.description("Добавление 1 книги в корзину с последующим изменением количества ее экземпляров и удалением из корзины")
 @allure.severity("critical")
 @allure.suite("UI тесты на работу с корзиной")
 def test_add_book():
     with allure.step("Создать экземпляр класса и открыть браузер"):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         main_page = MainPage(driver)
-    main_page.enter_values("удивительная девочка")
+        # main_page.cookies(cookie) кукисы не пробрасывались, добавлялся почему-то второй ключ в Application, по факту авторизации нет
+    main_page.enter_values("Идеальный Че, Интуиция и новые беспринцыпные истории")
     with allure.step("Добавить первую книгу из поисковой выдачи в корзину"):
         dct = main_page.add_to_cart()
     main_page.go_cart()
     book = main_page.check_book()
-    new_q = '5'
+    new_q = '8'
     main_page.change_quantity(new_q)
-    cart_q = main_page.check_quantity('5 товаров')
+    cart_q = main_page.check_quantity('8 товаров')
     main_page.del_book()
     clear_cart = main_page.check_quantity('0 товаров')
     with allure.step("Проверить, что в корзину добавилась нужная книга"):
-         assert book == dct["title"]
-    with allure.step("Проверить, что число экземпляров книги увеличилось в соответствии в выставленным значением"):
+        assert book == dct["title"]
+    with allure.step("Проверить, что число экземпляров книги увеличилось в соответствии с выставленным значением"):
         assert cart_q == new_q
-    with allure.step("Проверить, что книга удалилась из корзины"):
+    with allure.step("Проверить, книга удалилась из корзины"):
         assert clear_cart == '0'
 
-@allure.id("SKYPRO-6")
+
+@allure.id("SKYPRO-10")
 @allure.epic("Корзина Читай-город. UI")
 @allure.story("Добавление нескольких товаров в корзину")
 @allure.feature("ADD")
 @allure.title("Добавление 3 книг в корзину")
 @allure.description("Добавление книг из списка и подсчет общей стоимости покупки")
 @allure.severity("critical")
-@allure.suite("UI тесты на работу c корзиной")
+@allure.suite("UI тесты на работу с корзиной")
 def test_add_three_books():
     with allure.step("Создать экземпляр класса и открыть браузер"):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         main_page = MainPage(driver)
-    with allure.step("Добавить по очереди 3 книги в корзину через поиск"):
+        # main_page.cookies(cookie)
+    with allure.step("Добаввить по очереди 3 книги в корзину через поиск"):
         total_sum = 0
-    with allure.step("Выполнить цикл для поиска каждой книги по названию и последующего добавления в корзину"):
-        for i in ["1984", "Сумерки", "Цветы для Элджернона"]:
-            main_page.enter_values(i)
-            dct1 = main_page.add_to_cart()
-            total_sum += int(dct1["price"])
+        with allure.step("Выполнить цикл для поиска каждой книги по названию и последующего добавления в корзину первой книги из поисковой выдачи"):
+            for i in ["1984", "Сумерки", "Цветы для Элджернона"]:
+                main_page.enter_values(i)
+                dct1 = main_page.add_to_cart()
+                total_sum += int(dct1["price"])
     with allure.step("Перейти в корзину"):
         main_page.go_cart()
-    with allure.step("Записать в переменную общую стоимость покупки"):
-        cart_total = main_page.check_total()
+        with allure.step("Записать в переменную общую стоимость покупки"):
+            cart_total = main_page.check_total()
     with allure.step("Проверить, что сумма покупки совпадает с общей суммой трех добавленных книг"):
         assert total_sum >= int(cart_total)
